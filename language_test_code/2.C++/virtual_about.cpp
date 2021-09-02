b3547165589c30c4e33e6201607796198825d9a3
sunowsir <sunow.wang@gmail.com>
2021-09-02 20:54:11 +0800

    [add] add c++ virtual test code

---
+++ language_test_code/2.C++/virtual_about.cpp
@@ -0,0 +1,66 @@
+/*
+	* File     : virtual_about.cpp
+	* Author   : sunowsir
+	* Mail     : sunowsir@163.com
+	* Github   : github.com/sunowsir
+	* Creation : 2021年09月02日 星期四 16时03分30秒
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Animal {
+private:
+    int CName;
+public:
+    Animal(int New_Name);
+
+public:
+    virtual void output_test();
+    int get_cname();
+};
+
+typedef void (*vfunc)(void);
+
+
+Animal::Animal(int New_Name) : CName(New_Name) {
+};
+
+void Animal::output_test() {
+    std::cout << "Animal::output_test()" << std::endl;
+}
+
+int Animal::get_cname() {
+    return this->CName;
+}
+
+int main() {
+    /* 
+     * 通过地址访问类中的徐函数表、属性以及虚函数
+     */
+
+
+    Animal *ani = new Animal(1123);
+
+    /* 虚函数表的地址*/
+    vfunc **vtbptr_anim = (vfunc**)ani;
+    /* Animal::CName 的地址 */
+    int *cname_anim = (int*)((long*)ani + 1);
+    /* 虚函数表里第一个函数的地址 */
+    vfunc *vfunc_anim = *vtbptr_anim;
+
+    /* 输出以及调用测试 */
+
+    /* 通过对象调用public接口打印Animal::CName */
+    std::cout << "by ani->getcname() access Animal::CName : " << ani->get_cname() << std::endl;
+    /* 通过地址访问Animal::CName */
+    std::cout << "by address access Animal::CName : " << *(cname_anim) << std::endl;
+
+    /* 通过地址调用 Animal::output_test() */
+    std::cout << "by address call Animal::output_test() : " << std::endl;
+    (*vfunc_anim)();
+
+    return 0;
+}
+
